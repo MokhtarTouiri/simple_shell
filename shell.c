a12f2f881e7e9519d0690b5bd0443baa1a59ab67
chiheblahouli <chiheb.lahouli@gmail.com>
2020-04-14 20:30:35 +0000

    cleanup

--- shell.c
+++ shell.c
@@ -1,8 +1,32 @@
-#include "shell.h"
-
 /**
- *
- *
- *
+ * shell - the main shell program
+ * @ac: Number of command line args
+ * @av: Arguments
+ * @ev: Environment vars
+ * Return: 0 if success, code otherwise
  */
+
+int main(int ac, char **av, char **ev)
+{
+        sev_t sev;
+        int exitcode = 0;
+        (void)ac;
+
+        initialize_shell_env(&sev, av, ev);
+
+        while (sev.good2go)
+        {
+                display_prompt(sev);
+                getcommand(&sev);
+                check_alias(&sev);
+                if (!check_builtin(&sev))
+                        action(&sev);
+                display_error(&sev);
+        }
+        exitcode = sev.error;
+        clean_sev(&sev);
+        return (exitcode);
+}
+
+
 
