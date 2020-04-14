f9d852cf2869bb1baf964b434fe373cb42a4cb04
chiheblahouli <chiheb.lahouli@gmail.com>
2020-04-14 20:33:36 +0000

    cleanup

--- shell.c
+++ shell.c
@@ -8,24 +8,24 @@
 
 int main(int ac, char **av, char **ev)
 {
-        sev_t sev;
-        int exitcode = 0;
-        (void)ac;
+sev_t sev;
+int exitcode = 0;
+(void)ac;
 
-        initialize_shell_env(&sev, av, ev);
+initialize_shell_env(&sev, av, ev);
 
-        while (sev.good2go)
-        {
-                display_prompt(sev);
-                getcommand(&sev);
-                check_alias(&sev);
-                if (!check_builtin(&sev))
-                        action(&sev);
-                display_error(&sev);
-        }
-        exitcode = sev.error;
-        clean_sev(&sev);
-        return (exitcode);
+while (sev.good2go)
+{
+	display_prompt(sev);
+	getcommand(&sev);
+	check_alias(&sev);
+	if (!check_builtin(&sev))
+	action(&sev);
+	display_error(&sev);
+}
+exitcode = sev.error;
+clean_sev(&sev);
+return (exitcode);
 }
 
 
