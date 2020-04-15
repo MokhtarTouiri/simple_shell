ca057cc3221924035f3a7c238690474847a3f486
chiheblahouli <chiheb.lahouli@gmail.com>
2020-04-15 22:02:51 +0000

    mem fix

--- main.c
+++ main.c
@@ -1,66 +1,6 @@
 #include "shell.h"
 
 /**
- * new_exit - Builtin command exit
- * @av: arguments.
- * Return: Always 0.
- */
-
-int new_exit(char **av)
-{
-int code = 0;
-if (av[1] != NULL)
-	code = _atoi(av[1]);
-exit(code);
-}
-
-
-/**
- * new_env - Builtin command env
- *
- * Return: Return 1 if success.
- */
-
-void new_env(void)
-{
-int c = 0;
-while (environ[c])
-{
-	write(STDOUT_FILENO, environ[c], _strlen(environ[c]));
-	write(STDOUT_FILENO, "\n", 1);
-	c++;
-}
-
-}
-
-/**
- * execute_bl_in - Builtin command management
- *@av: arguments
- * Return: Return 1 if success.
- */
-
-int execute_bl_in(char **av)
-{
-char *cmd = av[0], *btin_cmd[] = {"env", "exit"};
-
-if (cmd == NULL)
-	return (1);
-
-if (strcmp(cmd, btin_cmd[0]) == 0)
-{
-	new_env();
-	return (1);
-}
-else if (strcmp(cmd, btin_cmd[1]) == 0)
-{
-	new_exit(av);
-	return (1);
-}
-return (0);
-}
-
-
-/**
  * main - the main shell program
  * Return: 0 on success code error if failure.
  */
